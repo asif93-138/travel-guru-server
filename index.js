be90aeede66947bf3ed2a9a41d94521cce82adb0
asif93-138 <asif93@student.sust.edu>
2023-11-13 21:13:10 +0600

    project done!

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const places = require('./places.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/data', (req, res) => {
+    res.send(places)
+  })
+
+app.get('/data/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('data for id :', id)
+    const place = places.find(x => x.id === id) || {};
+    res.send(place)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
